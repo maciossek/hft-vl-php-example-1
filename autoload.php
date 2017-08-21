7d8ad96cdd7cf3667bb4ef782992184f308624fc
Mathias Maciossek <mathias.maciossek@gmail.com>
2017-08-21 09:44:45 +0200

    initial commit

---
+++ autoload.php
@@ -0,0 +1,16 @@
+<?php
+
+function __autoload($classname) {
+    $filename = "./src/". $classname .".php";
+    include_once($filename);
+}
+
+// we've called a class ***
+
+
+$merkl = new Person("dumm", "wie brot" , 100);
+echo "<pre>";
+print_r($merkl);
+echo "</pre>";
+echo "Eine neue Person mit alter $merkl->age"
+?>
